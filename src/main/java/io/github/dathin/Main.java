85cf697a179cf64a82ad66a57656ba1b0dc37c99
Dathin <pedromcaires99@gmail.com>
2022-06-16 22:43:15 -0300

    lol

--- src/main/java/io/github/dathin/Main.java
+++ src/main/java/io/github/dathin/Main.java
@@ -14,6 +14,7 @@ public class Main {
         while (set.size() < 6) {
             var luckyNumber = random.nextInt(60);
             set.add(luckyNumber);
+            // I really hope this + 1 is the best idea instead of increase bounds and ignore 0. If someday all numbers turn out to be my number - 1 it would be very funy
             System.out.printf((luckyNumber + 1) + " | ");
         }
     }
